04f0bbc8f5b1a0eac854b00fbc86ada3b0dba18e
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-06-08 03:12:43 -0500

    added fahreinheit code and localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var todaysWeather = document.getElementById('Todays-Forecast');
 var APIKey = "fc68a64d0afee1bc09c4e15296f59f41";
-var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
 var cityName = document.getElementById('city-name');
 var currentIcon = document.getElementById('current-icon');
 var currentTemp = document.getElementById('temp');
 var currentWind = document.getElementById('wind-speed');
 var currentHumidity = document.getElementById('humidity');
 var currentUV = document.getElementById('UV-index');
+var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
+var history = document.getElementById('history')
+
 
 // Function to search weather by city and update card with response data
 function getCoords(city) {
@@ -25,7 +27,7 @@ function getCoords(city) {
         cityName.innerHTML = data.name + " "  + "-" + " " + month + "/" + day + "/" + year;
         var weatherIcon = data.weather[0].icon;
         currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-        currentTemp.innerHTML = "Temperature: " + data.main.temp;
+        currentTemp.innerHTML = "Temperature: " + data.main.temp + "&#8457";
         currentWind.innerHTML = "Wind Speed: " + data.wind.speed +" MPH";
         currentHumidity.innerHTML = "Humidity: " + data.main.humidity + "%";
         var lat = data.coord.lat;
@@ -40,10 +42,37 @@ function getCoords(city) {
     })
 }
 
-// Event Handler for Search Button
+// Function to list Recent Searches
+function listSearchHistory() {
+    history.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+        var recentCity = document.createElement('input');
+        recentCity.setAttribute('type', 'text');
+        recentCity.setAttribute('class', 'form-control d-block bg-grey');
+        recentCity.setAttribute('readonly', true);
+        recentCity.setAttribute('value', searchHistory[i]);
+        recentCity.addEventListener('click', function () {
+            getCoords(recentCity.value);
+        })
+    }
+}
+
+function init() {
+    listSearchHistory();
+}
+
+// Event Handler for 'Search' Button
 $('#searchBtn').click(function () {
     var city = $( ":input" ).val();
     getCoords(city);
     searchHistory.push(city);
     localStorage.setItem("search", JSON.stringify(searchHistory))
 })
+// Event Handler for 'Clear Recent Searches' Button
+$('#clearBtn').click(function () {
+    localStorage.clear();
+    searchHistory = [];
+    listSearchHistory();
+})
+
+init()
