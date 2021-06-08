39ea65ead4ecec02f466585f1ad07431f5863d60
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-06-08 16:51:13 -0500

    seperated .then functions into independant functions, set up readme, and trying to fix localstorage

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Weather-Dashboard
+# Weather Dashboard
+## Homework Week 6
+
+===
+
+## Description
+This project consists of a Weather Dashboard that dynamically updates HTML and CSS elements using JavaScript as well as API calls to the OpenWeather API.  The page utilizes OpenWeather API to allow the user to input a city which will return current information for the weather.  The API call then uses the city latitute and longitude returned in the first call to do another API call which returns UV-Index data and future forecast data.  When the page is loaded, recent search history is loaded from the local storage and displayed on the left side, that can also be used for faster input.  
+
+## Screenshot
+Here you can find a mockup screenshot of the webpage, displaying the Weather Dashboard in action:
+
+![screenshot](./assets/screenshot.PNG)
+---
+
+## URL
+Web page can be located at [Gitpages](https://mrg105.github.io/Weather-Dashboard/ "Weather Dashboard")
+
+## Repo
+Repository can be located at [Github](https://github.com/MrG105/Weather-Dashboard "Github")
+
+
+## Contact
+Javascript code and Repo updated by Gabriel Gutierrez.  [email](poo328@my.utsa.edu "email")
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,44 +7,86 @@ var currentWind = document.getElementById('wind-speed');
 var currentHumidity = document.getElementById('humidity');
 var currentUV = document.getElementById('UV-index');
 var searchHistory = JSON.parse(localStorage.getItem('search')) || [];
-var history = document.getElementById('history')
+var history = document.getElementById('history');
+var forecastHeader = document.getElementById('fiveDayForecast-header');
+var fiveDayForecast = document.querySelectorAll('.forecast');
 
 
 // Function to search weather by city and update card with response data
 function getCoords(city) {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey
     fetch(queryURL) 
-    .then(function (response) {
-        return response.json();
+    .then(function (response1) {
+        return response1.json();
     })
    
-    .then(function (data) {
-        todaysWeather.classList.remove('d-none');
-        var currentDate = new Date(data.dt * 1000);
-        var day = currentDate.getDate();
-        var month = currentDate.getMonth() + 1;
-        var year = currentDate.getFullYear();
-        cityName.innerHTML = data.name + " "  + "-" + " " + month + "/" + day + "/" + year;
-        var weatherIcon = data.weather[0].icon;
-        currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-        currentTemp.innerHTML = "Temperature: " + data.main.temp + "&#8457";
-        currentWind.innerHTML = "Wind Speed: " + data.wind.speed +" MPH";
-        currentHumidity.innerHTML = "Humidity: " + data.main.humidity + "%";
+    // OneCall API for UV Index and 5 Day Forecast
+    .then(function (data) {        
         var lat = data.coord.lat;
         var lon = data.coord.lon;
-        // OneCall API for UV Index and 5 Day Forecast
-        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=1" + "&appid=" + APIKey;
+        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=1" + "&units=imperial" + "&appid=" + APIKey;
         fetch(weatherURL)
-        .then(function (response) {
-            return response.json();
-            
-         })
+        .then(function (response2) {
+            setCurrentWeather(data);       
+            return response2.json();
+        })            
+            .then(function (data2) {
+                setUVIndex(data2);
+                setFiveDayForecast(data2);                 
+            })
     })
 }
+// sets Current Weather Card
+function setCurrentWeather(data) {
+    todaysWeather.classList.remove('d-none');
+    var currentDate = new Date(data.dt * 1000);
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    cityName.innerHTML = data.name + " "  + "-" + " " + month + "/" + day + "/" + year;
+    var weatherIcon = data.weather[0].icon;
+    currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+    currentTemp.innerHTML = "Temperature: " + data.main.temp + "&#8457";
+    currentWind.innerHTML = "Wind Speed: " + data.wind.speed +" MPH";
+    currentHumidity.innerHTML = "Humidity: " + data.main.humidity + "%";
+}
+// Function to set UV Index
+function setUVIndex (data) {
+    currentUV.innerHTML = "UV Index: " + data.current.uvi;
+    if (data.current.uvi <= 2) {
+        currentUV.setAttribute('class', 'badge badge-success');
+    } else if (data.current.uvi <= 5) {
+        currentUV.setAttribute('class', 'badge badge-warning');
+    } else {
+        currentUV.setAttribute('class', 'badge badge-danger');
+    }
+    
+}
 
+function setFiveDayForecast (data) {
+    forecastHeader.classList.remove('d-none');
+    for (i = 0; i <fiveDayForecast.length; i++) {
+        var forecastDate = new Date(data.dt * 1000);
+        var day = forecastDate.getDate();
+        var month = forecastDate.getMonth() + 1;
+        var year = forecastDate.getFullYear();
+        var dailyDate = document.createElement('h3')
+        dailyDate.innerHTML =  month + "/" + day + "/" + year;
+        var weatherIcon = data.daily[i].weather[0].icon;
+        var forecastIcon = document.createElement('img');
+        forecastIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+        var forecastTemp = document.createElement('p');
+        forecastTemp.innerHTML = "Temperature: " + data.daily[i].temp + "&#8457";
+        var forecastWind = document.createElement('p');
+        forecastWind.innerHTML = "Wind Speed: " + data.daily[i].wind_speed +" MPH";
+        var forecastHumidity = document.createElement('p');
+        forecastHumidity.innerHTML = "Humidity: " + data.daily[i].humidity + "%";
+        }
+
+
+}
 // Function to list Recent Searches
 function listSearchHistory() {
-    history.innerHTML = "";
     for (let i = 0; i < searchHistory.length; i++) {
         var recentCity = document.createElement('input');
         recentCity.setAttribute('type', 'text');
@@ -75,4 +117,4 @@ $('#clearBtn').click(function () {
     listSearchHistory();
 })
 
-init()
+init();
